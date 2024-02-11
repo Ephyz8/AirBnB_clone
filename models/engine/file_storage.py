a96b5d0f2f7a2d2e430a0fc92f3bc2b0d5f12ca3
Ephyz8 <ziwoyaephraim@gmail.com>
2024-02-10 17:38:25 -0800

    code cleaning

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,28 +7,29 @@ import os
 
 
 class FileStorage:
-    """Class to handle serialization and deserialisation of 
+    """Class to handle serialization and deserialisation of
     instances to and fro JSON file.
-    """ 
+    """
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
         """Returns the dictionary __objects"""
         return FileStorage.__objects
-    
+
     def new(self, obj):
         """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(ty(obj).__name, obj.id)
         FileStorage.__objects[key] = obj
-    
-   def save(self):
-       """Serializes __objects to the JSON file"""
-       with open(FileStorage.__file_path, "w", encoding="utf-8") as files:
-           docm = {ky: vl.to_dict() for ky, vl in FileStorage.__objects.items()}
-           json.dump(docm, files)
-    
-   def classes(self):
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as files:
+            docm = {ky: vl.to_dict()
+                    for ky, vl in FileStorage.__objects.items()}
+            json.dump(docm, files)
+
+    def classes(self):
         """Returns a dict of valid classes and their references"""
         from models.base_model import BaseModel
         from models.state import State
@@ -38,18 +39,17 @@ class FileStorage:
         from models.place import Place
         from models.user import User
 
-   def reload(self):
-       """Module to reload the stored objects."""
-       if not os.path.isfile(FileStorage.__file_path):
-           return
-       with open(FileStorage.__file_path, "r", encoding="utf-8") as files:
-           new_dict = json.load(f)
-           new_dict = {ky: self.classes()[vl["__class__"]]
-                       (**vl) for ky, vl in new_dict.items()}
-           FileStorage.__objects = new_dict
+    def reload(self):
+        """Module to reload the stored objects."""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as files:
+            new_dict = json.load(f)
+            new_dict = {ky: self.classes()[vl["__class__"]]
+                        (**vl) for ky, vl in new_dict.items()}
+            FileStorage.__objects = new_dict
 
-    
-   def attributes(self):
+    def attributes(self):
         """Returns the attrs. and their types for classname."""
         attributes = {
             "BaseModel":
