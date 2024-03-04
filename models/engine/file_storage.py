cf898eb37f864ccb6dd5289193e92d670168515b
Ephyz8 <ziwoyaephraim@gmail.com>
2024-03-04 02:10:45 +0200

    correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -71,11 +71,11 @@ class FileStorage:
             with open(FileStorage.__file_path, "r", encoding="utf-8") as files:
                 new_dict = json.load(files)
                 new_dict = {ky: self.classes()[vl["__class__"]](**vl) 
-                             for ky, vl in new_dict.items()
-                             if vl.get("__class__") in self.classes()}
+                            for ky, vl in new_dict.items() 
+                            if vl.get("__class__") in self.classes()}
                 FileStorage.__objects = new_dict
-        except Exception as e: 
-             pass
+        except Exception as e:
+          pass
 
     def attributes(self):
         """Returns the attrs. and their types for classname."""
