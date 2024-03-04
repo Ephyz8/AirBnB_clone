22f83198870b3cc1c466724bdce2e09664054b23
Ephyz8 <ziwoyaephraim@gmail.com>
2024-03-04 02:57:44 +0200

    correction

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """This is a basemodel script."""
 
 from uuid import uuid4
-import datetime
+from datetime import datetime
 from models import storage
 
 
@@ -21,18 +21,16 @@ class BaseModel:
         if kwargs is not None and kwargs != {}:
             for key in kwargs:
                 if key == "created_at":
-                    self.__dict__["created_at"] = datetime.datetime.strptime
-                    (kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["created_at"] = datetime.strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 elif key == "updated_at":
-                    self.__dict__["updated_at"] = datetime.datetime.strptime
-                    (kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                    self.__dict__["updated_at"] = datetime.strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
                 else:
                     self.__dict__[key] = kwargs[key]
 
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             storage.new(self)
 
     def __str__(self):
@@ -44,18 +42,15 @@ class BaseModel:
     def save(self):
         """Update the public instance attribute updated_at"""
 
-        self.updated_at = datetime.datetime.now()
+        self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
         """Returns a dictionary rep. of all keys/values of __dict__"""
 
         obj_dict = self.__dict__.copy()
-        if isinstance(obj_dict["created_at"], datetime.datetime):
-            obj_dict["created_at"] = obj_dict["created_at"].isoformat()
-        if isinstance(obj_dict["updated_at"], datetime.datetime):
-            obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
         obj_dict["__class__"] = type(self).__name__
-        '''obj_dict["created_at"] = obj_dict["created_at"].isoformat()
-        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()'''
+        obj_dict["created_at"] = obj_dict["created_at"].isoformat()
+        obj_dict["updated_at"] = obj_dict["updated_at"].isoformat()
         return obj_dict
+        
